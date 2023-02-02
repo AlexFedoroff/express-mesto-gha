13d7a8c30af49e246299237fbc272b22dcfa89a0
Alex Fedoroff <alexanderfedorov@192-168-1-6.local>
2023-02-02 16:04:22 +0300

    change_port

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+// const cors = require('cors');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 2800 } = process.env;
+// const allowedOrigins = ['http://localhost:5173'];
 const app = express();
 
 mongoose.set('strictQuery', false);
 
+app.use(requestLogger);
+/*
+app.use(cors({
+  origin: allowedOrigins,
+}));
+*/
 app.use(bodyParser.json());
 
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(error);
 
